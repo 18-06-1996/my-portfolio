0ccdcee11c9410b572697aab95d62d9e2e1b2cb1
18-06-1996 <karthick18696@gmail.com>
2023-07-10 14:08:32 +0530

    add images

--- src/Components/Projects/Projectcontainer.js
+++ src/Components/Projects/Projectcontainer.js
@@ -8,7 +8,7 @@ const Projectcontainer = () => {
 
 const projects =  [
   {
-    img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Flookaside.fbsbx.com%2Flookaside%2Fcrawler%2Fmedia%2F%3Fmedia_id%3D199302124882118&tbnid=c-_PAGlMsallCM&vet=12ahUKEwjD3NX03IOAAxVx0qACHSk3AkAQMygHegUIARDmAQ..i&imgrefurl=https%3A%2F%2Fwww.facebook.com%2F112351943577137%2Fphotos%2Fa.112357660243232%2F199302124882118%2F%3Ftype%3D3&docid=P-4JimB54ie1OM&w=512&h=512&q=shop%20zone%20logo&ved=2ahUKEwjD3NX03IOAAxVx0qACHSk3AkAQMygHegUIARDmAQ",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5Kn6KM3rKgvoDLAJ_zwAuJfmhgOfon0RGXA&usqp=CAU",
     title:"Shop Zone",
     desc:"Welocme to Shop zone. One of the wonderful online parchase platform.",
     link:"https://lustrous-dolphin-9e5959.netlify.app/",
