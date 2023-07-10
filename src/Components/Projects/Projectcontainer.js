ca54c37143d577f274f154cf2381f6d0ae761bd0
18-06-1996 <karthick18696@gmail.com>
2023-07-10 14:03:47 +0530

    logo change

--- src/Components/Projects/Projectcontainer.js
+++ src/Components/Projects/Projectcontainer.js
@@ -2,15 +2,15 @@ import React from 'react'
 import{Element} from 'react-scroll'
 import './Projectcontainer.css'
 import Project from '../Project/Project'
-import logo from  '../images/logo.png'
+
 
 const Projectcontainer = () => {
 
 const projects =  [
   {
-    img: {logo},
+    img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Flookaside.fbsbx.com%2Flookaside%2Fcrawler%2Fmedia%2F%3Fmedia_id%3D199302124882118&tbnid=c-_PAGlMsallCM&vet=12ahUKEwjD3NX03IOAAxVx0qACHSk3AkAQMygHegUIARDmAQ..i&imgrefurl=https%3A%2F%2Fwww.facebook.com%2F112351943577137%2Fphotos%2Fa.112357660243232%2F199302124882118%2F%3Ftype%3D3&docid=P-4JimB54ie1OM&w=512&h=512&q=shop%20zone%20logo&ved=2ahUKEwjD3NX03IOAAxVx0qACHSk3AkAQMygHegUIARDmAQ",
     title:"Shop Zone",
-    desc:"Welocme to Shop zone web application. One of the wonderful online parchase platform...!!! ",
+    desc:"Welocme to Shop zone. One of the wonderful online parchase platform.",
     link:"https://lustrous-dolphin-9e5959.netlify.app/",
   },
   {
